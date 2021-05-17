6c470782202dea3d1a15d58207ec1efd0b6d738d
TheIzvorg <TheIzvorg@DESKTOP-4IKOL7T>
2021-05-17 08:08:13 +0300

    Release version

--- TicTacToeCPP/Source.cpp
+++ TicTacToeCPP/Source.cpp
@@ -15,11 +15,11 @@ struct Coordinates {
 struct Options {
 	//массив с челиком —--------------------—
 	int map[3][3] = { {0,0,0,},
-						{0,0,0,},
-						{0,0,0,},
+					  {0,0,0,},
+					  {0,0,0,},
 	};
 
-	int menu[3] = { 1,0,0 };
+	// int menu[3] = { 1,0,0 };
 	int menuSelect = 0;
 };
 
@@ -45,6 +45,7 @@ void Draw(Options& options) {
 			options.map[i][j] = 0;
 		}
 	}
+	system("CLS");
 }
 
 bool DrawCheck(Options& options) {
@@ -125,12 +126,10 @@ bool VictoryCheck(Options& options, bool Check) {
 
 bool VictoryCheck(Options& options, char symbol) {
 	if (symbol == 'X' || symbol == 'x') {
-		VictoryCheck(options, true);
-		return true;
+		return VictoryCheck(options, true);
 	}
 	else if (symbol == 'O' || symbol == 'o') {
-		VictoryCheck(options, false);
-		return true;
+		return VictoryCheck(options, false);
 	}
 	return false;
 }
@@ -162,7 +161,6 @@ void showMap(Options options) {
 			{
 				cout << "o ";
 			}
-
 		}
 		cout << "| " << endl;
 		cout << "  +---+---+---+" << endl;
@@ -188,7 +186,7 @@ void BOTRandomXY(Options& options) {
 	cout << endl;
 }
 
-void PlayerMove(Options& options) {
+void PlayerMove(Options& options, int player) {
 
 	// Логарифм золотого сечения ln(n)
 	Coordinates cord;
@@ -203,7 +201,7 @@ void PlayerMove(Options& options) {
 		cord.Y = cord.Y > 0 || cord.Y <= 3 ? cord.Y - 1 : cord.Y % 3;
 		// cord.X -= 1; cord.Y -= 1;
 		if (options.map[cord.X][cord.Y] == 0) {
-			options.map[cord.X][cord.Y] = 1;
+			options.map[cord.X][cord.Y] = player;
 			break;
 		}
 		cout << "[!]Введите координаты повторно\n";
@@ -227,30 +225,60 @@ void PlayerMove(Options& options) {
 	} */
 }
 
+void PlayerMove(Options& options, char symbol)
+{
+	if (symbol == 'X' || symbol == 'x') {
+		PlayerMove(options, 1);
+	}
+	else if (symbol == 'O' || symbol == 'o') {
+		PlayerMove(options, 2);
+	}
+}
+
 void Single(Options& options) {
-	bool isEnd = false;
 	showMap(options);
 	do {
-		PlayerMove(options);
-		//VictoryCheck(options, true); // Проверка на победу X
-		isEnd = VictoryCheck(options, 'X'); // Проверка на победу X
+		PlayerMove(options,'X');
+		// Проверка на победу X
+		if (VictoryCheck(options, 'X')) { break; }
 		BOTRandomXY(options);
-		//VictoryCheck(options, false); // Проверка на победу O
-		isEnd = VictoryCheck(options, 'O'); // Проверка на победу O
+		// Проверка на победу O
+		if (VictoryCheck(options, 'O')) { break; }
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
 		showMap(options);
-		isEnd = DrawCheck(options);
-	} while (!isEnd);
+		if (DrawCheck(options)) { break; }
+	} while (true);
 }
 
 void Duo(Options& options) {
-
+	bool isXO = true;
+	showMap(options);
+	do {
+		string move = isXO ? "Крестики" : "Нолики";
+		char check = isXO ? 'X' : 'O';
+		cout << "Ходят - " << move << "!" << endl;
+		PlayerMove(options, check);
+		// Проверка на победу X
+		if (VictoryCheck(options, check)) { break; }
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+		showMap(options);
+		isXO = !isXO;
+		/*
+		cout << "Ходят - Нолики!" << endl;
+		PlayerMove(options, 'O');
+		// Проверка на победу O
+		if (VictoryCheck(options, 'O')) { break; }
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+		showMap(options);*/
+		if (DrawCheck(options)) { break; }
+	} while (true);
 }
 
 void showMenu(Options& options) {
 	while (true) {
 		cout << "[!]КРЕСТИКИ-НОЛИКИ[!]\n";
 		char checker = options.menuSelect == 0 ? '/' : ' ';
-		cout << "[" << checker << "] Одиночная игра" << endl;
+		cout << "[" << checker << "] Один игрок" << endl;
 		checker = options.menuSelect == 1 ? '/' : ' ';
 		cout << "[" << checker << "] Два игрока" << endl;
 		checker = options.menuSelect == 2 ? '/' : ' ';
@@ -260,6 +288,7 @@ void showMenu(Options& options) {
 		case 'W':
 		case 'ц':
 		case 'Ц':
+		case 72:
 			if (options.menuSelect - 1 >= 0)
 				options.menuSelect--;
 			break;
@@ -267,6 +296,7 @@ void showMenu(Options& options) {
 		case 'S':
 		case 'ы':
 		case 'Ы':
+		case 80:
 			if (options.menuSelect + 1 <= 2)
 				options.menuSelect++;
 			break;
@@ -292,7 +322,6 @@ int main()
 {
 	setlocale(0, "");
 
-
 	void* handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO structCursorInfo;
 	GetConsoleCursorInfo(handle, &structCursorInfo);
@@ -300,10 +329,8 @@ int main()
 	SetConsoleCursorInfo(handle, &structCursorInfo);
 
 	Options options;
-	bool isStop = true;
-	//showMap(options);
 
-	for (; isStop;) {
+	for (;;) {
 		//SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 }); // Ломает абсолютно всё, а переделывать леть
 		showMenu(options);
 	}
